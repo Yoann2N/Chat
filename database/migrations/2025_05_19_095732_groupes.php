1e5ca047d47eff178a91c552499c564be3376880
yoann <yoanngervais.yg13750@gmail.com>
2025-05-19 13:57:31 +0200

    cor

--- database/migrations/2025_05_19_095732_groupes.php
+++ database/migrations/2025_05_19_095732_groupes.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('description')->nullable();
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
-            $table->string ('NbrUser')->nullable();
+            $table->int ('NbrUser')->nullable();
 
         });
     }
